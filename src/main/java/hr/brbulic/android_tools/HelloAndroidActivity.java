0cac076b310cef7e9c20a004507670e41334a631
Bruno Bulic <brbulic@gmail.com>
2011-09-24 16:02:38 +0200

    Added experimental activity loading to Main Activity

--- src/main/java/hr/brbulic/android_tools/HelloAndroidActivity.java
+++ src/main/java/hr/brbulic/android_tools/HelloAndroidActivity.java
@@ -1,96 +1,131 @@
 package hr.brbulic.android_tools;
 
-import hr.brbulic.concurrency.BackgroundWorker;
-import hr.brbulic.concurrency.interfaces.IBackgroundDelegate;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import hr.brbulic.concurrency.BackgroundWorker;
+import hr.brbulic.concurrency.interfaces.IBackgroundDelegate;
+import hr.brbulic.factory.ActivityIntentFactory;
+import hr.brbulic.factory.intent.IntentSelector;
 
 public class HelloAndroidActivity extends Activity implements
-		IBackgroundDelegate {
+        IBackgroundDelegate, View.OnClickListener {
 
-	private TextView _myTextview;
-	private static String TAG = "android-tools";
+    private TextView _myTextview;
+    private Button _loadButton;
+    private static String TAG = "android-tools";
 
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Log.i(TAG, "onCreate");
-		setContentView(R.layout.main);
+    /**
+     * Called when the activity is first created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being
+     *                           shut down then this Bundle contains the data it most recently
+     *                           supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+     *                           is null.</b>
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        setContentView(R.layout.main);
 
-		_myTextview = (TextView) this.findViewById(R.id.mainTextview);
+        _myTextview = (TextView) this.findViewById(R.id.mainTextview);
+        _loadButton = (Button) this.findViewById(R.id.LoadOptionsButton);
+        _loadButton.setOnClickListener(this);
 
-		BackgroundWorker.getInstance().EnqueueSimple(this, "THIS IS MY STRING");
-		BackgroundWorker.getInstance().EnqueueRunnable(new MyHandler());
-		Toast.makeText(getApplicationContext(), TAG, 2);
-	}
+        BackgroundWorker.getInstance().EnqueueSimple(this, "THIS IS MY STRING");
+        BackgroundWorker.getInstance().EnqueueRunnable(new MyHandler());
+        Toast.makeText(getApplicationContext(), TAG, 2);
+    }
 
-	private class MyHandler implements Runnable {
+    @Override
+    public void onClick(View view) {
+        /*
+        Intent intent = new Intent(this.getApplicationContext(),OptionsActivity.class);
+        startActivity(intent);*/
 
-		@Override
-		public void run() {
-			int count = 5;
+        Parcelable myObj = new Parcelable() {
 
-			StringBuilder mainString = new StringBuilder();
+            @Override
+            public int describeContents() {
+                return 0;  //To change body of implemented methods use File | Settings | File Templates.
+            }
 
-			while (count < 10) {
-				String string = String.format("Ovo je prekršilo sve... %1$d",
-						count);
+            @Override
+            public void writeToParcel(Parcel parcel, int i) {
+                parcel.writeString("Ovo je neka moja baza");
+            }
+        };
 
-				mainString.append(string);
+        try {
+            ActivityIntentFactory.LoadActivity(this, IntentSelector.OPTIONS_SCREEN, myObj);
+        } catch (ClassNotFoundException e) {
+            Log.e(TAG, "Cannot load activity! Error:" + e.getLocalizedMessage());
+        }
 
-				Log.i(TAG, string);
-				try {
-					Thread.sleep(250);
-				} catch (InterruptedException e) {
-				}
+    }
 
-				count++;
-			}
+    private class MyHandler implements Runnable {
 
-			final String resultString = mainString.toString();
+        @Override
+        public void run() {
+            int count = 5;
 
-			_myTextview.post(new Runnable() {
+            StringBuilder mainString = new StringBuilder();
 
-				@Override
-				public void run() {
-					_myTextview.setText(resultString);
+            while (count < 10) {
+                String string = String.format("Ovo je prekrÅ¡ilo sve... %1$d",
+                        count);
 
-				}
-			});
-		}
+                mainString.append(string);
 
-	}
+                Log.i(TAG, string);
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+                }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+                count++;
+            }
 
-		BackgroundWorker.getInstance().stop();
-	}
+            final String resultString = mainString.toString();
 
-	@SuppressWarnings("hiding")
-	@Override
-	public <String> void backgroundRequest(String internalState) {
-		int count = 0;
+            _myTextview.post(new Runnable() {
 
-		System.out.println(internalState);
+                @Override
+                public void run() {
+                    _myTextview.setText(resultString);
 
-		while (count < 10) {
-			System.out.println("Just a message with number " + count);
-			count++;
-		}
-	}
+                }
+            });
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        BackgroundWorker.getInstance().stop();
+    }
+
+    @SuppressWarnings("hiding")
+    @Override
+    public <String> void backgroundRequest(String internalState) {
+        int count = 0;
+
+        System.out.println(internalState);
+
+        while (count < 10) {
+            System.out.println("Just a message with number " + count);
+            count++;
+        }
+    }
 
 }
